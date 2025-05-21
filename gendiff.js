9c2188fd5d6e299593c1481978a5c9666d662eb8
Dmitry Fedorov <dmitry.fedorov1704@gmail.com>
2025-05-21 06:07:12 +0300

    Step_2

--- gendiff.js
+++ gendiff.js
@@ -5,7 +5,11 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+    .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
+    .version('1.0.0')
+    .argument('<filepath1>', 'first configuration file')
+    .argument('<filepath2>', 'second configuration file')
+    .option('-f, --format [type]', 'output format');
 
 program.parse();
